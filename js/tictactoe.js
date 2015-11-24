2ec6aa2656b79aa97546eebce6b303c54651522a
Robin Arida <roar@splitmedialabs.com>
2015-11-24 23:04:28 +0800

    refactor: updated data with local storage and fixes some bugs

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,32 +6,37 @@ Vue.component('box', {
 
 new Vue ({
 	el: '#app',
-	data: {
+/*	data: {
 		board: [['', '', ''], ['', '', ''], ['', '', '']],
 		cplayer: '1',
 		players: { '1': { turn: 'X', name: 'Robin' }, '2': {turn: 'O', name: 'Arida'} },
-		moves: '0',
-		value: ''
-	},
-	watch: {
-		board: function() {
-			this.checkWinner();
-		}
+		moves: '0'
+	},*/
+	data: {
+		board: getState('board').length ? getState('board') : [['', '', ''], ['', '', ''], ['', '', '']],
+		cplayer: getState('cplayer').length ? getState('cplayer') : '1',
+		players: getState('players').length ? getState('players') : { '1': { turn: 'X', name: 'Robin', score: 0 }, '2': {turn: 'O', name: 'Arida', score: 0} },
+		moves: getState('moves').length ? getState('moves') : 0
 	},
 	methods: {
-/*		switchTurn: function() {
-			this.players['1'] = this.
-		}*/
+		switchTurn: function() {
+			var temp = this.players['1'];
+/*			this.players.$set('1', this.players['2']);
+			this.players.$set('2', temp);*/
+			this.players['1'] = this.players['2'];
+			this.players['2'] = temp;
+		},
 		claim: function(x, y) {
 			if(this.board[x][y] === '') {
 				this.board[x].$set(y, this.players[this.cplayer].turn);
+				this.moves++;
+				this.checkWinner();
 				this.nextTurn();
 			} else {
 				alert('This box is already taken!');
 			}
 		},
 		nextTurn: function() {
-			this.moves++;
 			this.cplayer = this.cplayer === '1' ? '2' : '1';
 		},
 		checkWinner: function() {
@@ -54,6 +59,37 @@ new Vue ({
 		reset: function() {
 			this.board = [['', '', ''], ['', '', ''], ['', '', '']];
 			this.moves = 0;
+			this.switchTurn();
+			this.cplayer = '1';
 		}
+	},
+	ready: function() {
+		this.$watch('board', function(value) {
+			saveState('board', value);
+		});
+		this.$watch('cplayer', function(value) {
+			saveState('cplayer', value);
+		});
+		this.$watch('players', function(value) {
+			alert('test');
+			saveState('players', value);
+		});
+		this.$watch('moves', function(value) {
+			saveState('moves', value);
+		});
 	}
 });
+
+function getState(key) {
+	if(localStorage.getItem(key)) {
+		return JSON.parse(localStorage.getItem(key));
+	}
+	return [];
+}
+
+function saveState(key, value) {
+	localStorage.setItem(key, JSON.stringify(value));
+	if(key === 'players') {
+		alert(JSON.stringify(value));
+	}
+}
